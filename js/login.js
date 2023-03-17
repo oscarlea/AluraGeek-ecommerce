df83c5fc5772034cb79e92961132eb1fa8326dcf
oscarlea <oscarlea@yahoo.com>
2023-03-17 14:17:55 -0300

    Listo Pag ingreso de producto

--- js/login.js
+++ js/login.js
@@ -1,12 +1,14 @@
 function login() {
-    var user, ássword
+    var user, password
 
     user = document.getElementsByName("email")[0].value
     password = document.getElementsByName("password")[0].value
 
     if (user == "prueba" && password == "1234") {
         alert("ya")
+        window.location.href = "nuevo.html";
     } else {
         alert("none")
+        window.location.href = "nuevo.html";
     }
 }
